6429b6b2f2bef7ac8963fc2d537d846f17523504
jm-888 <jm22186@my.westlake.school.nz>
2024-10-25 15:08:42 +1300

    remaking python

--- app.py
+++ app.py
@@ -1,53 +1,71 @@
-# Importing flask module in the project is mandatory
+ # Importing flask module in the project is mandatory
 #Render template is used to load in HTML files
 from flask import Flask, render_template,request, redirect, url_for, flash
 import random
 #Render template is used to load in HTML files
 import sqlite3
+import random
 
 # We use this to set up our flask sever
 app = Flask(__name__)
-
-# The route( tells the application which URL should call 
 # the associated function.
 @app.route('/')
 # ‘/’ URL is bound with index() function.
 def index():
     return render_template("index.html")
+#start
+###
+#
 
 
+def get_db_connection():
+    conn = sqlite3.connect('login.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+    
 
-@app.route('/edit_timetable', methods=['GET', 'POST']) 
-def edit(): 
-    if request.method == 'POST': 
-        DayOne = request.form['d1p1'] 
-        d1p2 = request.form['d1p2'] 
-        d1p3 = request.form['d1p3'] 
-        d1p4 = request.form['d1p3'] 
-        d1p5 = request.form['d1p4'] 
-  
-        with sqlite3.connect("TimeTable.db") as users: 
-            cursor = users.cursor() 
-            cursor.execute("INSERT INTO DayOne\
-(Period 1,Period 2,Period 3,Period 4,Period 5,Period 6,Period 7,) VALUES (?,?,?,?,?),(Period 1,Period 2,Period 3,Period 4,Period 5,Period 6,Period 7,"))
-            users.commit() 
-        return render_template("index.html") 
-    else: 
-        return render_template('edit_timetable.html') 
-  
-  
-@app.route('/timetable') 
-def timetable_display(): 
-    connect = sqlite3.connect('TimeTable.db') 
-    cursor = connect.cursor() 
-    cursor.execute('SELECT * FROM sqlite_master;') 
-    data = cursor.fetchall() 
-    return render_template("timetable.html", data=data) 
+@app.route('/edit_timetable', methods=('POST', 'GET'))
+def login():
+    if request.method == 'POST':
+        user_name = request.form['userName']
+        password = request.form['password']
 
-  
+        if not user_name or not password: 
+            flash('All Fields required')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO users (username, password) VALUES (?,?)', (user_name, password))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template("index.html")
+
+def edit_timetable(id):
+    conn = get_db_connection()
+    day1 = conn.execute('SELECT * FROM DayOne WHERE id=?', (id,)).fetchone()
+
+    if request.method == 'POST':
+        d1p1 = request.form['']
+        d1p2 = request.form['']
+        d1p3 = request.form['']
+        d1p4 = request.form['']
+        d1p5 = request.form['']
+        d1p6 = request.form['']
+        d1p7 = request.form['']
+
+        if not user_name or not password: 
+            flash('All fields are required!')
+        else:
+            conn.execute('UPDATE users SET username = ?, password = ? WHERE id = ?', 
+                (user_name, password, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('admin'))
+            
+    return render_template('edit_timetable.html', day1=day1)
+
+    
 
 # main driver function #MAKE SURE THIS STAYS AT THE BOTTOM AT ALL TIMES
 if __name__ == '__main__':
     app.run(debug=True,port=5448)
-
-
